ministryofjustice/logstash-formula

commit d722af2df65d835dd35bdf33f2cdf3d74071e171

Prevent cleaning of conf.d dir from causing reload
Finally worked this out. In order to clean a directory out but
leave files there that are needed, you must *require* the files
in the `file.directory`.

This however creates a chicken-vs-egg problem, as you are effectively
requiring a file to be present before the directory.

To hack around this, we add the 'makedirs: True' option to our files,
which removes this limitation.

This is documented in https://github.com/saltstack/salt/issues/12965

NB: In this particular instance it makes more sense for us to `require_in`
the clean directory in each file. It has the same effect though.

logstash/logstash.sls
@@ -89,31 +89,29 @@ logstash_archive_log_dir:
     - require:
       - pkg: logstash_indexer
 
-/etc/logstash/conf.d:
+clean_logstash_confd_dir:
   file.directory:
+    - name: /etc/logstash/conf.d
     - clean: True
     - dir_mode: 2755
     - user: root
     - group: logstash
-    - require:
-      - pkg: logstash_indexer
 
 {% for conf_file in logstash.config_files %}
 
 /etc/logstash/conf.d/{{ conf_file }}:
-  file:
-    - managed
+  file.managed:
     - source: salt://logstash/templates/logstash/conf.d/{{ conf_file }}
     - template: jinja
+    - makedir: True
     - mode: 644
     - user: root
     - group: logstash
     - mode: 644
     - require:
       - pkg: logstash_indexer
-      - file: /etc/logstash/conf.d
-    - watch_in:
-      - service: logstash_indexer
+    - require_in:
+      - file: clean_logstash_confd_dir
 
 {% endfor %}
 

commit b09d380cf8ffd2f3455cdebf94b2d62417ab8e82

Fix issue with logstash restarting
A bug was introduced in d722af2df65d835dd35bdf33f2cdf3d74071e171
where logstash service would not restart after config change.

logstash/logstash.sls
@@ -112,6 +112,8 @@ clean_logstash_confd_dir:
       - pkg: logstash_indexer
     - require_in:
       - file: clean_logstash_confd_dir
+    - watch_in:
+      - service: logstash_indexer
 
 {% endfor %}
 